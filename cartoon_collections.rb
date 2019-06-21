2e7f738c82ede9802444a00b9bc95810aad05bf7
Grace Colette <ilovegrace09@gmail.com>
2019-06-21 04:33:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,19 +18,18 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  words = calls_long.length
-  if words < 4
-    calls_long.include? do |words| words < 4
-  end
-else
-  true
+  # words = calls_long.length
+  # binding.pry
+  # if words < 4
+    calls_long.any? do |words| words.length > 4
+#   end
+# else
+#   true
   end
 end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if foods.include?(cheese_types.join)
-    cheese_types.find {|cheese| foods.index(1) == cheese_types}
-  else
+    cheese_types.find {|cheese| foods.include?(cheese)}
   end
-end
+
