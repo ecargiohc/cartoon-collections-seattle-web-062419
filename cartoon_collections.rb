83836cc05220609dede3e8bb8ed0690eb9be13e3
Grace Colette <ilovegrace09@gmail.com>
2019-06-21 04:13:45 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index| 
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# def reverse_each_word(sentence)
+#   words = sentence.split(" ")
+#   words.collect do |word| 
+#     word.reverse 
+#   end.join(" ")
+# end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |planeteer| 
+    planeteer.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  words = calls_long.length
+  if words < 4
+    calls_long.include? do |words| words < 4
+  end
+else
+  true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if foods.include?(cheese_types.join)
+    cheese_types.find {|cheese| foods.index(1) == cheese_types}
+  else
+  end
 end
